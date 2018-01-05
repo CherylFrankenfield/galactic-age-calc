d15e5ba30a8e4c117a8bb18de9cebfbd21ad5027
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-01-05 09:47:07 -0800

    Add dist and clean tasks, install gulp-util, del and jshint packages and tools

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,17 @@ const concat = require('gulp-concat');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const uglify = require('gulp-uglify');
+const jshint = require('gulp-jshint');
+const utilities = require('gulp-util');
+const del = require('del');
+const distProduction = utilities.env.production;
 
 
-
-
-
-
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -27,4 +32,19 @@ gulp.task('minifyScripts', ['jsBrowserify'], function(){
   return gulp.src("./dist/js/app.js")
   .pipe(uglify())
   .pipe(gulp.dest("./dist/js"));
+});
+
+
+
+
+gulp.task('clean', function(){
+  return del(['dist', 'tmp']);
+});
+
+gulp.task('dist', ['clean'], function(){
+  if (distProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
 });
